9b2ed25c36e69f11ca3acf9e18d149fea60fa86a
Hany Hamed <h.hamed.elanwar@gmail.com>
2019-11-24 14:28:24 +0300

    Fix priest table

--- schema.sql
+++ schema.sql
@@ -425,7 +425,9 @@ CREATE TABLE nurses_schedule(
 CREATE TABLE priests_schedule(
     date DATE,
     patient_id INT,
-    FOREIGN KEY (patient_id) REFERENCES patient(id)
+    priest_id INT,
+    FOREIGN KEY (patient_id) REFERENCES patient(id),
+    FOREIGN KEY (priest_id) REFERENCES priest(id)
 );
 
 CREATE TABLE patient_allergies(
